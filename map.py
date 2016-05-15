9beb4e1a168e09682fe613d80f10b6508d67e829
Joshua Olson <0joshua.olson1@gmail.com>
2016-05-15 08:11:53 -0600

    Comment for once

--- map.py
+++ map.py
@@ -1,22 +1,22 @@
 notesPerSide, middle = 18, "A"
 
 from decimal import Decimal
-newLeft = newRight = 60
+newLeft = newRight = 60 #MIDI middle C
 leftDs = [2, 2, 1, 2, 2, 1, 2]
 rightDs = [2, 1, 2, 2, 1, 2, 2]
 dOld = 0
-for i in xrange("ABCDEFG".index(middle)):
-  leftDs = leftDs[1:] + leftDs[:1]
-  rightDs = rightDs[1:] + rightDs[:1]
+for i in range("ABCDEFG".index(middle)):
+    leftDs = leftDs[1:] + leftDs[:1]
+    rightDs = rightDs[1:] + rightDs[:1]
 func = {}
-for i in xrange(notesPerSide + 2):
-  func[62 - dOld] = newLeft
-  func[62 + dOld] = newRight
-  newLeft -= leftDs[0]
-  newRight += rightDs[0]
-  leftDs = leftDs[-1:] + leftDs[:-1]
-  rightDs = rightDs[1:] + rightDs[:1]
-  dOld += 23.0 / notesPerSide
+for i in range(notesPerSide + 2):
+    func[62 - dOld] = newLeft #the center key of half sizes isn't middle C
+    func[62 + dOld] = newRight
+    newLeft -= leftDs[0]
+    newRight += rightDs[0]
+    leftDs = leftDs[-1:] + leftDs[:-1]
+    rightDs = rightDs[1:] + rightDs[:1]
+    dOld += 23.0 / notesPerSide #there are 23 default keys each side of center
 for note in sorted(list(func)):
-  formatted = str(Decimal(note).quantize(Decimal("1.000")))
-  print formatted + ", " + str(func[note]) + ".000"
+    formatted = str(Decimal(note).quantize(Decimal("1.000")))
+    print(formatted + ", " + str(func[note]) + ".000")
